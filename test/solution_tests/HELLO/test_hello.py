c297ee5de3270b01a1c8c23833fb51b244e1c727
sbx_user1051 <sbx_user1051@169.254.19.218>
2024-12-02 19:46:37 +0000

    Mon Dec 02 19:46:37 UTC 2024

--- test/solution_tests/HELLO/test_hello.py
+++ test/solution_tests/HELLO/test_hello.py
@@ -1,7 +1,8 @@
 from lib.solutions.HLO import hello_solution
 
 
-class TestSum():
+class TestHello:
     def test_hello(self):
         friend_name = 'sai'
         assert hello_solution.hello(friend_name) == f"Hello, {friend_name}!"
+
