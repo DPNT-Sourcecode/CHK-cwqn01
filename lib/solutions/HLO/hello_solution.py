2dd829427e89f45466ffb960ac2b58aba2ff3ca9
sbx_user1051 <sbx_user1051@169.254.19.218>
2024-12-02 19:52:37 +0000

    Mon Dec 02 19:52:37 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,10 +1,6 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    if not isinstance(friend_name, str):
-        raise ValueError("Input must be a string.")
-    if not friend_name.isalpha():
-        raise ValueError("Input must contain only alphabetic characters (including Unicode letters).")
+def hello(param):
+    return param
 
-    return f"Hello, world"
 
--- test/solution_tests/HELLO/test_hello.py
+++
@@ -1,8 +0,0 @@
-from lib.solutions.HLO import hello_solution
-
-
-class TestHello:
-    def test_hello(self):
-        friend_name = 'sai'
-        assert hello_solution.hello(friend_name) == f"Hello, {friend_name}!"
-
