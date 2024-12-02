e19fe7f86ea59dd063cce19ffc5a5726d5728294
sbx_user1051 <sbx_user1051@169.254.19.218>
2024-12-02 21:12:36 +0000

    Mon Dec 02 21:12:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -73,12 +73,13 @@ def checkout(skus):
         total_price += count * price_table[sku]["price"]
 
         # If no offers apply, simply add the regular price (items without "offers" or "special_offer")
-        if "offers" not in price_table[sku] and "special_offer" not in price_table[sku]:
+        if "offers" not in price_table[sku] or "special_offer" not in price_table[sku]:
             if count == 1:
                 total_price = price_table[sku]["price"]
             total_price += count * price_table[sku]["price"]
 
     return total_price
+
 
 
 
